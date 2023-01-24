ec5ba2632d3232a93506497d02fdbd0df49da37a
sheblight <sheblight@gmail.com>
2023-01-24 12:19:07 -0500

    Allow window toggling for TODO modal

--- src/index.js
+++ src/index.js
@@ -1,2 +1,8 @@
 import style from "./style.css";
 
+// testing todo modal interactivity
+const todoModal = document.querySelector(".todo-modal-wrapper");
+const todoExit = document.querySelector("button.todo-close");
+const entry = document.querySelector(".todo-entry");
+todoExit.onclick = ()=>{todoModal.classList.add("hidden")};
+entry.onclick = ()=>{todoModal.classList.remove("hidden")};
